f74cefc9f78eb7aa5be19eae5602900f678a0569
Meghan <meghan@calcium75.com>
2024-06-03 12:10:43 -0700

    Add postgress database connection
    • create .env
    • add to .gitignore
    • update dependencies via npm i @vercel/postgres
    https://nextjs.org/learn/dashboard-app/setting-up-your-database

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a4881af5f3af3d14"; }
+/******/ 		__webpack_require__.h = function() { return "a21dc6a568efc59a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
